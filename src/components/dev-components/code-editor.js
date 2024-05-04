e7620cd67479553edc4ff3b68ce9504dee10f891
yuyan7498 <100181293+yuyan7498@users.noreply.github.com>
2024-05-04 10:43:15 +0800

    顏色改變

--- src/components/dev-components/code-editor.js
+++ src/components/dev-components/code-editor.js
@@ -4,9 +4,11 @@ import { Box } from '@mui/material';
 export default function ThreeEqualBoxes() {
     return (
         <Box sx={{ width: '100%', height: '500px', display: 'flex' }}>
-            <Box sx={{ width: '33.333%', height: '100%', backgroundColor: '#FF5733' }} />
-            <Box sx={{ width: '33.333%', height: '100%', backgroundColor: '#33C1FF' }} />
-            <Box sx={{ width: '33.333%', height: '100%', backgroundColor: '#8E44AD' }}></Box>
+            <Box sx={{ width: '33.333%', height: '100%', backgroundColor: '#FF5733' }}></Box>
+            <Box sx={{ width: '33.333%', height: '100%', backgroundColor: '#33C1FF' }}></Box>
+            <Box sx={{ width: '33.333%', height: '100%', backgroundColor: '#8E44AD' }}>
+                
+            </Box>
         </Box>
     );
 }
