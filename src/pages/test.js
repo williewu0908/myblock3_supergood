492427a6b4ef4ac8a9d4de96b7eaaba7c2152755
yuyan7498 <100181293+yuyan7498@users.noreply.github.com>
2024-05-27 10:28:03 +0800

    font awesome svg core

--- src/pages/test.js
+++ src/pages/test.js
@@ -1,6 +1,5 @@
 import DevNavBar from "@/components/dev-components/dev-nav";
 import CodeEditor from "@/components/dev-components/code-editor";
-import PythonFlowchart from "@/components/flowchart/pythonFlowchart";
 
 export default function Test() {
     return (
