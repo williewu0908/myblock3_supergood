e9f3674930a7740d0f95aa0af20d92ccaec8ae13
yuyan7498 <100181293+yuyan7498@users.noreply.github.com>
2024-10-25 13:54:35 +0800

    推推

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,41 +12,6 @@ export default function Index() {
     Code: true,
     ChatWithAI: true,
   });
-  const router = useRouter();
-
-  useEffect(() => {
-    const checkAuth = async () => {
-      const username = localStorage.getItem('username');
-      const email = localStorage.getItem('email');
-      const encoded_data = localStorage.getItem('encoded_data');
-
-      if (!username || !email || !encoded_data) {
-        router.push('/login');
-        return;
-      }
-
-      try {
-        const response = await fetch('http://127.0.0.1:5000/auth/check-auth', {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify({ username, email, encoded_data }),
-        });
-
-        if (response.status === 401) {
-          router.push('/login');
-        } else if (!response.ok) {
-          throw new Error('Network response was not ok');
-        }
-      } catch (error) {
-        console.error('Error checking authentication:', error);
-        router.push('/login');
-      }
-    };
-
-    checkAuth();
-  }, [router]);
 
   const toggleViewState = (newState) => {
     setViewState(newState);
