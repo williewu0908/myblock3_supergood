2da65745faa69cf2d58d475f24110f0bdfa7d118
yuyan7498 <100181293+yuyan7498@users.noreply.github.com>
2024-11-13 22:24:43 +0800

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    assetPrefix: process.env.NODE_ENV === 'production' ? 'https://sw-hie-ie.nknu.edu.tw' : '',
+    basePath: process.env.NODE_ENV === 'production' ? '/myblock3c' : '',
 };
 
 export default nextConfig;
