e2d76cb7206136f00339149e84f5c496b53ca5e8
Tsai, Yu-Kun <bond920213@gmail.com>
2024-11-16 06:42:46 +0800

    補上一個操作的描述 Merge branch 'AI-feature'
    # Conflicts:
    #	src/components/chatAI/ChatInterface/ChatInterface.js

--- src/components/pythonEditor/pythonEditor.js
+++ src/components/pythonEditor/pythonEditor.js
@@ -185,7 +185,7 @@ function PythonEditor() {
                     model: "Llama3-8B"
                 };
     
-                const response = await fetch("/api/chat", {
+                const response = await fetch("http://127.0.0.1:5000/generate-answer", {
                     method: "POST",
                     headers: {
                         "Content-Type": "application/json"
