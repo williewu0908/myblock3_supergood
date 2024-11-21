1f67b9fd6173e24482b665ebb617f1396412a7fe
yuyan7498 <100181293+yuyan7498@users.noreply.github.com>
2024-11-21 23:59:26 +0800

    修正渲染問題

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,13 @@
 import '@/components/global-components/global.css';
+import { XMLProvider } from "@/components/blockly/XMLContext";
+import { CodeProvider } from '@/components/dev-components/CodeContext'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <XMLProvider>
+      <CodeProvider>
+        <Component {...pageProps} />
+      </CodeProvider>
+    </XMLProvider>
+  );
 }
