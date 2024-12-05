3db6e8303129c1d880ce1e37104435caf6bb77d7
yuyan7498 <100181293+yuyan7498@users.noreply.github.com>
2024-12-06 01:14:45 +0800

    路由改變

--- src/components/dev-components/dev-nav.js
+++ src/components/dev-components/dev-nav.js
@@ -94,7 +94,7 @@ export default function DevNavBar({ toggleViewState, handlegenerateXML }) {
     React.useEffect(() => {
         const checkAuth = async () => {
             try {
-                const response = await fetch('/whois', {
+                const response = await fetch('/myblock3/whois', {
                     method: 'GET',
                     credentials: 'include' // 確保攜帶 cookie
                 });
