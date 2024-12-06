c60372234625e295bc63fddcc714e9654951e30c
Tsai, Yu-Kun <bond920213@gmail.com>
2024-12-07 05:58:31 +0800

    footer

---
+++ src/components/footer/footer.js
@@ -0,0 +1,32 @@
+export default function Footer() {
+  return (
+    <footer
+      className="footer"
+      style={{
+        display: "block",
+        width: "100%",
+        textAlign: "center",
+        background: "#4b5c66",
+        color: "white",
+        lineHeight: 1.5,
+        fontFamily: "'Poppins', sans-serif",
+        padding: "15px 0",
+      }}
+    >
+      <p>&emsp;</p>
+      <p>
+        <a style={{ color: "white" }} href="https://sites.google.com/mail.nknu.edu.tw/cph/home">
+          🌏HIE Lab | Ⓖ 🚀 Ⓖ | 🛸 Ⓖ | 🚜 Ⓦ&emsp;
+        </a>
+      </p>
+      <p>
+        <a style={{ color: "white" }} href="https://sites.google.com/mail.nknu.edu.tw/iecnknu/%E9%A6%96%E9%A0%81">
+          &emsp;🌏NKNU-IEC Ⓖ Ⓕ Ⓑ
+        </a>
+      </p>
+      <p>© 2008-2025 Power by Po-Hsun Cheng (鄭伯壎) and Li-Wei Chen (陳立偉),</p>
+      <p>Information Education Center, National Kaohsiung Normal University, Taiwan.</p>
+      <p>Source: Yu-Kun Tsai (蔡煜堃), XXX-XXX XXX (陳彥宇), XXX-XXX XXX (吳威廷)</p>
+    </footer>
+  );
+}
