cbce65ca70e47426fa49fdcbcde9c2917585da81
WillieWu <itri464687@itri.org.tw>
2025-12-10 10:53:57 +0800

    next.js改standalone模式

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export',
+    output: 'standalone',
     basePath: process.env.NODE_ENV === 'development' ? '' : '/myblock3c',
     assetPrefix: process.env.NODE_ENV === 'development' ? '' : '/myblock3c/',
     trailingSlash: true,
